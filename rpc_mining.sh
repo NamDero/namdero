71600d6ff0503cbeb9d6153d30b26f386abd60c5
NamDero <130159869+NamDero@users.noreply.github.com>
2023-04-08 09:18:26 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    dero1qy8zhemxcvd4wgvt83njypvp6cw0lx432ftmhznau0sdzyhzfatlsqg79c9hw -r 192.168.1.119:10100 -r1 community-pools.mysrv.cloud:10300 -p rpc;
+    dero1qy3xmyv0ld0hk7wlfhh04x4q72jaj2lpsxnjnxjj926ahe42jjezqqgfz4s97 -r 192.168.1.119:10100 -r1 community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
